558ee5d2c774f144f811fc530877788fafa9609a
dan <danpprince@gmail.com>
2015-02-24 17:18:20 -0500

    Effect parameters put in separate file to simplify changes in version control.

--- chorus_effect.m
+++ chorus_effect.m
@@ -1,13 +1,7 @@
 close all; clear all; clc
 
-%% Parameters
-	filename = 'Joey sassy guitar.wav';
-	delay_length     = 0.025;  % sec
-	modulation_depth = 0.007;  % sec
-	modulation_rate  = 0.25;   % Hz
-	feedback         = 0.50;   % Percent
-	dry_wet_balance  = 0.35;   % 0.0 for all dry, 1.0 for all wet
-%%
+% Load effect parameters from other file
+chorus_effect_parameters;
 
 [input, sample_rate] = audioread(filename);
 
---
+++ chorus_effect_parameters.m
@@ -0,0 +1,8 @@
+%% Parameters
+	filename = 'Joey sassy guitar.wav';
+	delay_length     = 0.025;  % sec
+	modulation_depth = 0.007;  % sec
+	modulation_rate  = 0.25;   % Hz
+	feedback         = 0.50;   % Percent
+	dry_wet_balance  = 0.35;   % 0.0 for all dry, 1.0 for all wet
+%%
